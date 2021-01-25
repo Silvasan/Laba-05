27c4bebe2df86ca6bccf216382e4e50dc1caef53
Silvasan <74051655+Silvasan@users.noreply.github.com>
2021-01-26 01:38:09 +0300

    right free and malloc/realloc

--- Source.c
+++ Source.c
@@ -1,19 +1,24 @@
+
 #include <stdio.h> 
 #include <stdlib.h>
+
 char string(int, char);
 int main() {
-	int is_it_equal_words = 0, counter_letters = 0, words_count = 0, word_length, str_length;
-	char *str, *word;
+	int i=0,j=0,k=0, is_it_equal_words = 0, counter_letters = 0, words_count = 0, word_length, str_length;
 	printf("Enter a string:\n");
-	string(&str_length, &str);
+	char* str;
+	if(string(&str_length, &str) ==(char)-1)
+		return -1;
+	char* word;
 	printf("\nEnter a world:\n");
-	string(&word_length, &word);
-	for (int i = 0; str[i] != '\n'; i++) {
+	if (string(&word_length, &word) == (char)-1)
+		return -1;
+	for (; str[i] != '\0'; i++) {
 		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z')) {
 			counter_letters++;
 			if (!((str[i + 1] >= 'A' && str[i + 1] <= 'Z') || (str[i + 1] >= 'a' && str[i + 1] <= 'z'))) {
 				if (counter_letters == word_length) {
-					for (int j = i - (counter_letters-1), k = word_length-1;  j <= i; j++, k--) {
+					for (j = i - (counter_letters-1), k = word_length-1;  j <= i; j++, k--) {
 						if (str[j] == word[k]) {
 							is_it_equal_words++;
 							if (is_it_equal_words == counter_letters) {
@@ -32,27 +37,29 @@ int main() {
 	}
 	printf("\nResult: %d", words_count);
 	free(str);
-	str[3];
-	while (str != NULL)
-		free(str);
-	for (int i = 0; i < word_length; i++)
-		free(word[i]);
+	str = NULL;
 	free(word);
+ 	word = NULL;
 	getchar();
 	return 0;
 }
+
 char string(int* len, char** array) {
 	*len = 0;
 	int maxlen = 1;
 	char *s = (char*)malloc(sizeof(char));//empty string
-
+	if (s == NULL) {
+		printf("ERROR. Can't allocate memory");
+		getchar();
+		return-1;
+	}
 	char letter = getchar();
 	while (letter != '\n') {
 		s[(*len)++] = letter;
 		if (*len >= maxlen) {
 			maxlen *= 2;
-			s = (char*)realloc(s, maxlen * sizeof(char));
-			if (*s == NULL) {
+			char *tmp = (char*)realloc(s, maxlen * sizeof(char));
+			if (tmp == NULL) {
 				printf("ERROR. Can't reallocate memory");
 				getchar();
 				return-1;
@@ -60,8 +67,7 @@ char string(int* len, char** array) {
 		}
 		letter = getchar();
 	}
-	s[*len] = '\n';
-	s[(*len)+1] = '\0';
+	s[*len] = '\0';
 	*array = s;
-	return 0;
+	return *array;
 }
